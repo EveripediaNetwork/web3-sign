d75425533bb4b602379025a3eb9b5c4f9dd560b7
Srujan Gurram <srujangs8@gmail.com>
2023-04-24 20:17:13 +0530

    Fix verify method test in verify.test.ts to properly test signature verification

--- src/__tests__/verify.test.ts
+++ src/__tests__/verify.test.ts
@@ -25,8 +25,7 @@ const defaultOptions = {
 describe('Verify method', () => {
   const real_address = client.account.address.toLowerCase()
 
-  //skip all test but the last
-  it.skip('must verify a signature', async () => {
+  it('must verify a signature', async () => {
     const token = await sign(
       (body) => client.signMessage({ account, message: body }),
       defaultOptions,
